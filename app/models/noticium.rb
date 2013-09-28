027576ecdb91e776b9436bd5c9dd823e4743e820
Facundo <rodrif89@gmail.com>
2013-09-28 11:54:49 -0300

    problemas con las pruebas

--- app/models/noticium.rb
+++ app/models/noticium.rb
@@ -1,2 +1,4 @@
 class Noticium < ActiveRecord::Base
+  validates :title, :presence => true, :length => { :minimum => 5 }
+  validates :content, :presence => true, :length => { :minimum => 10 }
 end
--- test/models/noticium_test.rb
+++ test/models/noticium_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class NoticiumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "validaciones" do
+    noticia = Noticia.new(:title => "My Book Title" , :content => "yyy")
+    assert product.valid?
+  end
+
 end
