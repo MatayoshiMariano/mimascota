3026686839398621a767efc78124db486499c0df
akodelia <ankodelia@gmail.com>
2013-10-10 00:00:45 -0300

    Se agrega un perfil y más datos en los formularios de registro

--- app/controllers/devise_overrides/registrations_controller.rb
+++ app/controllers/devise_overrides/registrations_controller.rb
@@ -3,7 +3,8 @@ class DeviseOverrides::RegistrationsController < Devise::RegistrationsController
 	before_filter :authenticate_user!
   
   def require_params
-  	 params.require(:user).permit( :email, :password, :password_confirmation, :first_name, :last_name)
+  	 params.require(:user).permit( :email, :password, :password_confirmation, :first_name, :last_name, 
+  	 	:birthday, :phone, :cellphone)
   end
 
   def devise_parameter_sanitizer
