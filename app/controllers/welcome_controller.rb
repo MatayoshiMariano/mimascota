8d8932f0cb9fbc2c0c45259984082959899188d5
MatayoshiMariano <matayoshi.mariano@gmail.com>
2013-11-06 11:58:19 -0300

    Info window created with link to the description view of the dog

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,31 +1,31 @@
 class WelcomeController < ApplicationController
 	def index
     @json = WantedDog.all.to_gmaps4rails do |wanted_dog, marker|
-      marker.infowindow render_to_string(partial: "info_window", locals: { object: wanted_dog })
+    marker.infowindow render_to_string(partial: "info_window", locals: {descr_url: wanted_dog_description_path, object: wanted_dog})
       marker.json({ category: 'WantedDog'})
     end
 	end
 
   def lost_dogs_json
-    dog_json(LostDog, 'LostDog')
+    dog_json(LostDog, 'LostDog', lost_dog_description_path)
   end
 
   def wanted_dogs_json
-    dog_json(WantedDog, 'WantedDog')
+    dog_json(WantedDog, 'WantedDog', lost_dog_description_path)
   end
 
   def adopt_dogs_json
-    dog_json(AdoptionDog, 'AdoptDogs')
+    dog_json(AdoptionDog, 'AdoptDogs', lost_dog_description_path)
   end
 
   def found_dogs_json
-    dog_json(FoundDog, 'FoundDogs')
+    dog_json(FoundDog, 'FoundDogs', lost_dog_description_path)
   end
 
 private
-  def dog_json(dogType, category)
+  def dog_json(dogType, category, description_url)
     dogs = dogType.all.to_gmaps4rails do |dog, marker|
-      marker.infowindow render_to_string(partial: "info_window", locals: { object: dog })
+      marker.infowindow render_to_string(partial: "info_window", locals: {descr_url: description_url, object: dog})
       marker.json({ category: category})
     end
     render json: dogs        
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Mimascota::Application.routes.draw do
   get 'welcome/adopt_dogs_json' => 'welcome#adopt_dogs_json'
   get 'welcome/found_dogs_json' => 'welcome#found_dogs_json'
 
+  get 'lost_dog/description' => 'lost_dog#description'
+  get 'wanted_dog/description' => 'wanted_dog#description'
+  get 'adopton_dog/description' => 'adopton_dog#description'
+  get 'found_dog/description' => 'found_dog#description'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
