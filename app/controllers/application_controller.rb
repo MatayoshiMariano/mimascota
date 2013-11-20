4a54c69e55435c50ce5fb46e7ce2f3d90212039e
MatayoshiMariano <matayoshi.mariano@gmail.com>
2013-11-20 11:02:08 -0300

    Comments sanitizer, it makes the sign in doesnt work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
 
  protected
  
-  def devise_parameter_sanitizer
-    if resource_class == User
-      User::ParameterSanitizer.new(User, :user, params)
-    else
-      super
-    end
-  end
+  # def devise_parameter_sanitizer
+  #   if resource_class == User
+  #     User::ParameterSanitizer.new(User, :user, params)
+  #   else
+  #     super
+  #   end
+  # end
 end
