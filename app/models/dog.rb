8308cfbf2d6b231b2e4106e9234de2ac825dcab3
MarianoMatayoshi <matayoshi.mariano@gmail.com>
2014-03-12 11:51:12 -0300

    iADAD

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -17,4 +17,8 @@ module Dog
     #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
     "#{self.latitude}, #{self.longitude}"
   end
+
+  def aa
+    true
+  end
 end
